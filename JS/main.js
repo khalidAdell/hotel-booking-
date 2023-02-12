ea93c44b75e270ff42854b22ed3f0da5d86d2da3
khalidAdell <kadel2034@gmail.com>
2023-02-13 00:10:31 +0200

    navBar & home page

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,18 @@
-"use strict"
+"use strict";
+
+//-------- Nav Bar--------//
+
+let nav = document.querySelector("nav");
+let main = document.querySelector("main");
+let showNavBtn = document.querySelector(".nav-list");
+let closeNavBtn = document.querySelector(".nav-close");
+
+function closeNav() {
+  nav.style = `right : -18rem;`;
+}
+function showNav() {
+  nav.style = `right : 0;`;
+}
+main.addEventListener("click", closeNav);
+closeNavBtn.addEventListener("click", closeNav);
+showNavBtn.addEventListener("click", showNav);
